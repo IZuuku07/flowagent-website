f8f6a12eb0eba7b4fbd17aaa5027055b4ce562c6
FlowAgent <jotaroe007@gmail.com>
2026-06-22 18:03:26 +0100

    Delete flowagent-pro-html directory

--- flowagent-pro-html/removeLogs.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('script.js', 'utf8');
-code = code.replace(/console\.log\([^)]*DEBUG[^)]*\);/g, '');
-fs.writeFileSync('script.js', code);
-console.log('Logs removed');
